7107c4e68971e5abc0a334840e0845be7bc4ecf8
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-10-21 18:15:05 +0100

    problem solved in class

--- src/screens/Auth/Class.js
+++ src/screens/Auth/Class.js
@@ -15,81 +15,23 @@ const Class = () => {
 
 export default Class;
 
-const styles = StyleSheet.create({});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const FashionPage = () => {
-const [data, setData] = useState([]);
-
-console.log('sssssssss', data)
-
-// Fetch data from API (replace with your fashion API)
-const fetchData = async () => {
-    try {
-        const response = await fetch('https://fakestoreapi.com/products');
-        const json = await response.json();
-        setData(json.items); // Assuming API returns an array of fashion items
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-useEffect(() => {
-    fetchData();
-}, []);
-
-// Render each item
-const renderItem = ({ item }) => (
-    <View style={styles.itemContainer}>
-        <Image source={{ uri: item.image }} style={styles.image} />
-        <Text style={{ color: 'black', fontSize: 11 }}>{item.title}</Text>
-    </View>
-);
-
-return (
-    <View style={{ flex: 1, backgroundColor: 'white', }}>
-        <FlatList
-            data={data}
-            renderItem={renderItem}
-            numColumns={3} // Display items in 3 columns
-        />
-    </View>
-);
-// };
-
-// const styles = StyleSheet.create({
-//     itemContainer: {
-//         borderWidth: 1,
-//         width: SIZES.h1 * 4,
-//         height: SIZES.h1 * 4,
-//         borderColor: COLORS.black,
-//         alignItems: 'center',
-//     },
-//     image: {
-//         // width: 100,
-//         height: 150,
-//         resizeMode: 'cover',
-//     },
-//     title: {
-//         marginTop: 5,
-//         fontSize: 16,
-//     },
-// });
-
-// export default FashionPage;
+const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+        backgroundColor: '#f1f1f1',
+        paddingTop: 20,
+        padding: 20,
+        paddingHorizontal: 20,
+    },
+    container: {
+        borderRadius: 10,
+        marginTop: 9,
+        height: 400,
+        borderWidth: 1,
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+});
