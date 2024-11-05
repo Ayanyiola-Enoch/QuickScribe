c7345f44e1746133d630431f045184d512930603
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-11-05 17:00:29 +0100

    Layout complete

--- src/screens/Auth/Note.js
+++ src/screens/Auth/Note.js
@@ -1,9 +1,12 @@
 import { Image, StyleSheet, FlatList, TouchableOpacity, TextInput, Text, View } from 'react-native'
-import React from 'react';
+import React, { useState } from 'react';
 import { COLORS, FONTS, icons, images, SIZES } from '../../constants';
 import { useNavigation } from '@react-navigation/native';
 
 const Note = () => {
+    const [input, setInput] = useState('');
+    const [store, setStore] = useState('');
+
     const navigation = useNavigation();
     return (
         <View style={styles.page}>
@@ -23,23 +26,30 @@ const Note = () => {
                         return (
                             <TouchableOpacity style={{ borderRadius: 5, borderWidth: 1, width: '100%', height: SIZES.h1 * 1.8, marginTop: SIZES.h2, justifyContent: 'center', paddingHorizontal: SIZES.h4 }}>
                                 <View style={{ flexDirection: 'row', alignItems: 'center', flex: 1 }}>
-                                    <Text style={{ ...FONTS.body3 }}>First todo</Text>
-                                    <View style={{ borderRadius: 5, borderWidth: 1, width: SIZES.h4, height: SIZES.h4, alignItems: 'center' }}>
+                                    <View style={{ flex: 1, }}>
+                                        <Text style={{ ...FONTS.body3 }}>First todo</Text>
                                     </View>
 
-                                    <Image source={images.deleteImg} style={{ width: SIZES.h2 }} />
+                                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', gap: 5 }}>
+                                        <TouchableOpacity>
+                                            <View style={{ borderRadius: 5, borderWidth: 1, width: SIZES.h3, height: SIZES.h3, alignItems: 'center' }}></View>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity>
+                                            <Image source={icons.deleteImg} style={{ width: SIZES.h2, height: SIZES.h2 }} />
+                                        </TouchableOpacity>
+                                    </View>
                                 </View>
                             </TouchableOpacity>
-                        )
+                        );
                     }} />
             </View>
 
             {/* bottom adding input */}
 
-            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-betweens' }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
                 <TextInput placeholder='Enter a new To-do' style={styles.text} />
                 <TouchableOpacity style={{ borderWidth: 1, borderRadius: SIZES.h1 * 5, padding: SIZES.h5 }}>
-                    <Image source={icons.add} style={{ width: SIZES.h1 * 0.8, height: SIZES.h1 * 0.8, tintColor: COLORS.primary, }} />
+                    <Image source={icons.add} style={{ width: SIZES.h1 * 0.6, height: SIZES.h1 * 0.6, tintColor: COLORS.primary, }} />
                 </TouchableOpacity>
             </View>
         </View>
@@ -59,8 +69,8 @@ const styles = StyleSheet.create({
     text: {
         borderWidth: 1,
         borderRadius: 5,
-        width: SIZES.h1 * 8.5,
-        height: SIZES.h1 * 1.6,
+        width: SIZES.h1 * 8.7,
+        height: SIZES.h1 * 1.5,
         paddingHorizontal: SIZES.h4,
     }
 });
