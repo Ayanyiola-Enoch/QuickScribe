d9ef4618ab003b13fabb7073ae24d45b67eae0ea
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-11-30 07:41:36 +0100

    still implementing

--- Redux.js
+++ Redux.js
@@ -16,15 +16,18 @@ const changeName = (firstname) => {
 };
 
 // REDUCERS
-const reducers = (state, action) => {
+const reducers = (state = initialState, action) => {
     switch (action.type) {
         case "change":
             return {
                 ...state,
                 firstName: action.firstName,
-            }
+            };
 
         default:
-
+            return state;
     }
 };
+
+const store = createStore(reducers);
+console.log('initialState', store.getState());
---
+++ testing.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { store } from './Redux'
+import { Provider, useSelector } from 'react-redux';
+import { FONTS, } from './src/constants';
+
+const AppContent = () => {
+  const enoch = useSelector((kk) => kk.firstName);
+
+  console.log('.....////', enoch);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Hello, {enoch}!</Text>
+    </View>
+  );
+
+};
+
+
+
+
+
+
+
+const testing = () => {
+  return (
+    <Provider store={store}>
+      <AppContent />
+    </Provider>
+  )
+}
+
+export default testing;
+
+const styles = StyleSheet.create({})
